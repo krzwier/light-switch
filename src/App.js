404054733703246ff098c01a21cd3ea0ee2dfb73
krzwier <krzwier@gmail.com>
2021-06-02 15:50:09 -0500

    Adds light switch toggle method

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  state = { on: true };
-  render() {
-    let wallClass = "wall off";
-    if (this.state.on) {
-      wallClass = "wall on";
+    state = { on: true };
+
+    render() {
+        let wallClass = "wall off";
+        if (this.state.on) {
+            wallClass = "wall on";
+        }
+        return (
+            <div className={wallClass}>
+                <div className="switch-plate">
+                    <div className="screw" />
+                    <div className="switch" onClick={this.toggle}>
+                        <div className="switch-handle" />
+                    </div>
+                    <div className="screw" />
+                </div>
+            </div>
+        );
     }
-    return (
-      <div className={wallClass}>
-        <div className="switch-plate">
-          <div className="screw" />
-          <div className="switch">
-            <div className="switch-handle" />
-          </div>
-          <div className="screw" />
-        </div>
-      </div>
-    );
-  }
+
+    toggle = () => {
+        this.setState({ on: !this.state.on });
+    }
+
 }
+
+
 
 export default App;
